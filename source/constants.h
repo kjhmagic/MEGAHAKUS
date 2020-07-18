262706d0427b2e789fde10e3deb3868665ce0b4d
kjhmagic <68441599+kjhmagic@users.noreply.github.com>
2020-07-19 00:54:47 +0900

    Update constants.h
    Update some OFFSET address.
    Comment out other OFFSET address.

--- source/constants.h
+++ source/constants.h
@@ -6,16 +6,16 @@
 #define GAME_TITLE_ID 0x100F3100DA46000
 
 // Disable NPR
-#define ADD_PARAM_OFFSET 0x99BA41
+#define ADD_PARAM_OFFSET 0x9A6F30
 const std::string add_param_redirect="adp_force";
 const std::string add_param_original="add_param";
 
 // VP
-#define VP_AMOUNT_OFFSET_MAIN 0x4D0E69F8
+#define VP_AMOUNT_OFFSET_MAIN 0x4D0F49F8
 
 // Debug mode
 // Thanks to samyuu for the addresses and the InputState struct
-#define SHOW_SPRITES_OFFSET 0x1CDD58
+/*#define SHOW_SPRITES_OFFSET 0x1CDD58
 const unsigned char show_sprites_enabled[]={0xF3, 0x03, 0x00, 0x2A};
 const unsigned char show_sprites_disabled[]={0xE0, 0x07, 0x00, 0x32};
 #define DW_GUI_UPDATE_OFFSET 0x2DAB2F
@@ -32,7 +32,7 @@ const unsigned char input_poll_enabled[]={0x36, 0xF8, 0xFF, 0x97};
 const unsigned char input_poll_disabled[]={0x1F, 0x20, 0x03, 0xD5};
 
 // Map mouse to touchscreen
-/*#define MOUSE_MAP_OFFSET 0x1CB83D
+#define MOUSE_MAP_OFFSET 0x1CB83D
 const unsigned char mouse_map_default=0xA1;
 const unsigned char mouse_map_touchscreen=0xE9;
 
@@ -43,16 +43,16 @@ const unsigned char mouse_map_disabled[]={0x04, 0x00, 0x00, 0x14};*/
 
 // Resolution scale
 // Thanks to SwigS-27 for the offset
-#define RES_SCALE_OFFSET 0xB2D191BC
+#define RES_SCALE_OFFSET 0xB2D271DC
 
 // Enable recording
 // Using somewhatlurker's method
-#define ENABLE_RECORDING_OFFSET 0x61EBA0
+/*#define ENABLE_RECORDING_OFFSET 0x61EBA0
 const unsigned char recording_enabled[]={0x1F, 0x20, 0x03, 0xD5};
-const unsigned char recording_disabled[]={0x41, 0x00, 0x00, 0x36};
+const unsigned char recording_disabled[]={0x41, 0x00, 0x00, 0x36};*/
 
 // Hide PV watermark
 // Offset found by M&M
-#define PVMARK_OFFSET 0x97981D
+#define PVMARK_OFFSET 0x984C21
 
 #endif // CONSTANTS_H
